82d68ce3c9b3db50677f5dd3a6f669987dc95c02
Jacob Scocca <131225995+jcscocca@users.noreply.github.com>
2026-06-24 14:16:37 -0700

    feat: add map-first styling foundation and fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "leaflet/dist/leaflet.css";
 import "./styles.css";
+import "./styles/mapWorkspace.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
